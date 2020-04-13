c26d066f69c088c25d5bc0a080b047f615594418
Sahil Bansal <shlbnsl843@gmail.com>
2020-04-13 17:25:58 +0530

    Ace IDE works

--- src/app/ide/ide.component.ts
+++ src/app/ide/ide.component.ts
@@ -1,6 +1,15 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Directive, OnInit, ViewChild, ElementRef } from '@angular/core';
 import { Input } from '@angular/core'
 import { Output, EventEmitter } from '@angular/core';
+import * as ace from 'ace-builds'; // ace-module
+
+// language package, choose your own 
+import 'ace-builds/src-noconflict/mode-javascript';
+// ui-theme package
+import 'ace-builds/src-noconflict/theme-github';
+
+const THEME = 'ace/theme/github';
+const LANG = 'ace/mode/javascript';
 
 @Component({
   selector: 'app-ide',
@@ -9,9 +18,23 @@ import { Output, EventEmitter } from '@angular/core';
 })
 export class IdeComponent implements OnInit {
 
+  @ViewChild('codeEditor', {static: true}) codeEditorElmRef: ElementRef;
+  private codeEditor: ace.Ace.Editor;
   constructor() { }
 
   ngOnInit() {
+    const element = this.codeEditorElmRef.nativeElement;
+    const editorOptions: Partial<ace.Ace.EditorOptions> = {
+      highlightActiveLine: true,
+      minLines: 10,
+      maxLines: Infinity,
+      fontSize: 32,
+    };
+
+    this.codeEditor = ace.edit(element, editorOptions);
+    this.codeEditor.setTheme(THEME);
+    this.codeEditor.getSession().setMode(LANG);
+    this.codeEditor.setShowFoldWidgets(true);
   }
 
 }

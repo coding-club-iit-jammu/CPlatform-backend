3be745d513262dd5d7b8813df1ca77365f7b4015
prateek18597 <prateek18597@gmail.com>
2020-04-19 13:53:48 +0530

    Storing only latest submission

--- CodingPlatformServer/controllers/assignment.js
+++ CodingPlatformServer/controllers/assignment.js
@@ -4,6 +4,7 @@ const mkdirp = require('mkdirp');
 
 const Assignment = require('../models/assignment');
 const Course = require('../models/course');
+const Submission = require('../models/submission');
 
 exports.getAssignmentDoc = (req, res, next) => {
     const assignmentId = req.query.assignmentId;
@@ -55,14 +56,35 @@ exports.submitAssignment = async (req,res,next) => {
     const assignmentId = req.body.assignmentId;
     const email = req.userEmail;
 
-    let assignment = await Assignment.findById(assignmentId);
+    let assignment = await Assignment.findById(assignmentId)
+                    .populate({ path:'submissions',
+                                model:'Submission',
+                                match: {
+                                    email : email
+                                }
+                    });
 
     if(!assignment){
         res.status(404).json({message:"Try Again"});
     }
 
-    if(req.file){
+    console.log(assignment['submissions'])
 
+    if(assignment['submissions'].length > 0){
+        if(!req.file){
+            res.status(400).json({message:"Try Again"});
+            return;
+        }
+        
+        const id = assignment['submissions'][0]['_id'];
+        let oldSubmission = await Submission.findById(id);
+
+        if(!oldSubmission){
+            res.status(404).json({message:"Try Again"});
+            return;
+        }
+
+        const oldUrl = oldSubmission['submissionUrl'];
         const fileName = path.basename(req.file.path);
         let oldPath = req.file.path;
         let newPath = path.join(__dirname,'..','data',req.body.courseCode,assignment.title,email).toString();
@@ -74,18 +96,60 @@ exports.submitAssignment = async (req,res,next) => {
         await fs.renameSync(oldPath, newPath, function (err) {
             if (err) {
                 moved = false;
-                // throw err
             }
         })
 
         if(!moved)
             newPath = oldPath;
+        
+        oldSubmission.submissionTime = new Date().toLocaleString('en-In');
+        oldSubmission.submissionUrl = newPath;
 
-        assignment.addSubmission(email,newPath).then((result)=>{
-            res.status(201).json(result);
-        });
+        let result = await oldSubmission.save();
+        
+        if(!result){
+            res.status(404).json({message:"Try Again"});
+            return;
+        }
+
+        fs.unlinkSync(oldUrl, function (err) {
+            if (err) {
+                console.log(`Unable to delete file ${oldUrl}`);
+            }
+            console.log('File deleted!');
+        }); 
+
+        res.status(201).json({message:'Assignment Submitted.'});
+
     } else {
-        res.status(400).json({message:"Try Again"});
+        if(req.file){
+        
+            const fileName = path.basename(req.file.path);
+            let oldPath = req.file.path;
+            let newPath = path.join(__dirname,'..','data',req.body.courseCode,assignment.title,email).toString();
+            
+            await mkdirp(newPath);
+            newPath = path.join(newPath,fileName);
+    
+            let moved = true;
+            await fs.renameSync(oldPath, newPath, function (err) {
+                if (err) {
+                    moved = false;
+                    // throw err
+                }
+            })
+    
+            if(!moved)
+                newPath = oldPath;
+    
+            assignment.addSubmission(email,newPath).then((result)=>{
+                res.status(201).json(result);
+            });
+        } else {
+            res.status(400).json({message:"Try Again"});
+        }
     }
+
+
     
 }
---
+++ CodingPlatformServer/data/2019_02_CSL106/Assignment 4/2016ucs0049@iitjammu.ac.in/client-1587279362802.js
@@ -0,0 +1,8 @@
+var xhttp = new XMLHttpRequest();
+  xhttp.onreadystatechange = function() {
+    if (this.readyState == 4 && this.status == 200) {
+      console.log(this.responseText);
+    }
+  };
+  xhttp.open("GET", "/", true);
+  xhttp.send();

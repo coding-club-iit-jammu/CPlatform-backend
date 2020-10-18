6c1d246615452156ebb40e7fce923e9368618d9b
vishals0077 <63539639+vishals0077@users.noreply.github.com>
2020-10-18 22:11:10 +0530

    Delete updatesubmission.js

--- routes/updatesubmission.js
+++
@@ -1,7 +0,0 @@
-const express = require('express');
-const path = require('path');
-const CodeofIDEController = require('../controllers/CodeofIDE');
-const router = express.Router();
-
-router.put('/updatesubmission',CodeofIDEController.updatePrevSubmission);
-module.exports = router;

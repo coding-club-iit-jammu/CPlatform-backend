8590f31ff930b356a0979b07c698e2ae669a8850
vishals0077 <63539639+vishals0077@users.noreply.github.com>
2020-10-20 21:10:43 +0530

    Update getidecode.js

--- routes/getidecode.js
+++ routes/getidecode.js
@@ -5,7 +5,7 @@ const isAuth = require('../middleware/is-auth');
 const router = express.Router();
 
 router.get('/getidecode',isAuth, codeofIDEController.getCode);
-router.post('/saveidecode',isAuth codeofIDEController.saveCode);
+router.post('/saveidecode',isAuth, codeofIDEController.saveCode);
 router.put('/autosaveidecode',isAuth, codeofIDEController.autosave);
 
 module.exports = router;

b3a9709a1b06aac72701ffb12a2e536d8c0fc1fd
Abhishek Chaudhary <abhishekchaudhary0220@gmail.com>
2020-12-11 11:34:01 +0530

    deleted tokens

--- jwtConfig_temp.js
+++
@@ -1,4 +0,0 @@
-module.exports = {
-    accessTokenSecret: 'ThisIsASecretKeyPratikParmarASDFGHJKLZXCVBNMQWERTYUIOP',
-    refreshTokenSecret: 'ThisIsASecretKeySahilBansalASDFGHJKLZXCVBNMQWERTYUIOP'
-}
